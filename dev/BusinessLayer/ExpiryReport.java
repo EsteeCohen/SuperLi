97ee37132c8e020d3b84d7485e2738da7ca1e200
levido454 <idou@post.bgu.ac.il>
2025-04-15 15:55:50 +0300

    Added everything

--- dev/BusinessLayer/ExpiryReport.java
+++ dev/BusinessLayer/ExpiryReport.java
@@ -1,5 +1,11 @@
 package BusinessLayer;
 
-public class ExpiryReport {
+import java.time.LocalDate;
+import java.util.List;
 
+public class ExpiryReport
+{
+    private List<Product> products;
+    private LocalDate reportDate;
+    private List<String> categories;
 }
--- dev/BusinessLayer/ProductFacade.java
+++ dev/BusinessLayer/ProductFacade.java
@@ -1,5 +1,27 @@
 package BusinessLayer;
 
-public class ProductFacade {
+import java.util.Dictionary;
+import java.util.List;
 
+public class ProductFacade
+{
+    private Dictionary<String, List<Product>> productsByCategory;
+    private Dictionary<String, Product> productsByName;
+
+    void AddProduct(String name, List<String> subCategories, String manufacturer, int shelfQuantity, int storageQuantity, int minQuantity, double costPrice, double salePrice, List<Discount> discountHistory)
+    {
+
+    }
+    void SetDiscountForProduct(String productName)
+    {
+
+    }
+    void SetDiscountForCategory(String productCategory)
+    {
+
+    }
+    int GetQuantity(String productName)
+    {
+        return 0;
+    }
 }
