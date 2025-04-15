6bbf8a0c4722551a5798baadb333e53ee895e89d
david podvinsky <davidpod@post.bgu.ac.il>
2025-04-15 15:34:40 +0300

    added domain layer discount class

--- dev/BusinessLayer/Discount.java
+++ dev/BusinessLayer/Discount.java
@@ -1,5 +1,29 @@
 package BusinessLayer;
+import java.time.LocalDate;
 
-public class Discount {
+class Discount {
+    private final LocalDate _startDate;
+    private final LocalDate _endDate;
+    private final double _precentage;
+
+    Discount(LocalDate startDate, LocalDate endDate, double precentage)
+    {
+        _startDate=startDate;
+        _endDate=endDate;
+        _precentage=precentage;
+    }
+
+    LocalDate getStartDate()
+    {
+        return _startDate;
+    }
+    LocalDate getEndDate()
+    {
+        return _endDate;
+    }
+    double getPrecentage()
+    {
+        return _precentage;
+    }
 
 }
