6bdd02e4451722fd92d6c677894d91f905294899
NadavSvilim1 <nadavsvi@post.bgu.ac.il>
2025-04-17 18:46:18 +0300

    added DL to all DomainLayer objects

--- dev/src/domainLayer/EmployeeDL.java
+++ dev/src/domainLayer/EmployeeDL.java
@@ -1,35 +1,34 @@
 package domainLayer;
 
 import domainLayer.Enums.WageType;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Employee {
+public class EmployeeDL {
     private final String id;
     private final String password;
     private final  String fullName;
     private final LocalDate wordStartingDate;
     private final int wage;
-    private final WageTypeSL wageType;
-    private final List<RolePL> roles;
+    private final WageType wageType;
+    private final List<RoleDL> roles;
     private final int yearlySickDays;
     private final int yearlyDaysOff;
 
-    public Employee(String id, String password, String fullName, LocalDate wordStartingDate, int wage, char wageTypeChar, int yearlySickDays, int yearlyDaysOff) {
+    public EmployeeDL(String id, String password, String fullName, LocalDate wordStartingDate, int wage, char wageTypeChar, int yearlySickDays, int yearlyDaysOff) {
         this.id = id;
         this.password = password;
         this.fullName = fullName;
         this.wordStartingDate = wordStartingDate;
         this.wage = wage;
-        this.wageType = WageTypeSL.fromChar(wageTypeChar);
-        this.roles = new ArrayList<RolePL>();
+        this.wageType = WageType.fromChar(wageTypeChar);
+        this.roles = new ArrayList<RoleDL>();
         this.yearlySickDays = yearlySickDays;
         this.yearlyDaysOff = yearlyDaysOff;
     }
 
-    public boolean hasRole(RolePL role) {
+    public boolean hasRole(RoleDL role) {
         return roles.contains(role);
     }
 
--- dev/src/domainLayer/EmployeeFacade.java
+++ dev/src/domainLayer/EmployeeFacade.java
@@ -4,7 +4,7 @@ import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class EmployeeFacade {
-    private Dictionary<String, EmployeeSL> employees;
+    private Dictionary<String, EmployeeDL> employees;
 
     // Singleton
     private static EmployeeFacade instance = null;
@@ -16,21 +16,21 @@ public class EmployeeFacade {
     }
 
     private EmployeeFacade() {
-        this.employees = new Hashtable<String, EmployeeSL>();
+        this.employees = new Hashtable<String, EmployeeDL>();
     }
 
-    public void addEmployee(EmployeeSL e) {
+    public void addEmployee(EmployeeDL e) {
         if (e != null) {
             employees.put(e.getId(), e);
         }
     }
 
-    public EmployeeSL getEmployee(String id) {
+    public EmployeeDL getEmployee(String id) {
         return employees.get(id);
     }
 
     public boolean employeeHasRole(String id, String roleName) {
-        EmployeeSL employee = employees.get(id);
+        EmployeeDL employee = employees.get(id);
         if (employee != null) {
             return employee.hasRole(RoleFacade.getInstance().getRoleByName(roleName));
         }
