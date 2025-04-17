820cad30d4b2448f57a23ed859f0f6a586606c53
Naama <shlittna@post.bgu.ac.il>
2025-04-17 20:04:40 +0300

    added files to presentation

--- dev/src/serviceLayer/RoleService.java
+++ dev/src/serviceLayer/RoleService.java
@@ -7,13 +7,13 @@ public class RoleService {
     private RoleFacade roleFacade;
 
     public List<RoleSL> getAllRoles() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAvailableRoles'");
+        return roleFacade.getAllRoles().stream()
+                .map(RoleSL::new)
+                .toList();
     }
 
-    public void createRole(RoleSL role) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createRole'");
+    public void createRole(String roleName) {
+       roleFacade.add(new RoleSL(roleName));
     }
 
 }
