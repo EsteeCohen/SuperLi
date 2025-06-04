5a7482c999d74739a045a93220b9a47b8bd9a965
NadavSvilim1 <nadavsvi@post.bgu.ac.il>
2025-06-04 11:38:46 +0300

    added intefaces

--- dev/employeeDev/src/serviceLayer/Interfaces/ITransportScheduleService.java
+++ dev/employeeDev/src/serviceLayer/Interfaces/ITransportScheduleService.java
@@ -4,12 +4,6 @@ import java.time.LocalDateTime;
 import transportDev.src.main.entities.Site;
 
 public interface ITransportScheduleService {
-    /**
-     * Returns a list of transport schedules that are planned to arrive within the specified time range.
-     *
-     * @param startDate the start of the time range
-     * @param endDate the end of the time range
-     * @return a list of upcoming deliveries scheduled between startDate and endDate
-     */
+
     boolean areThereArivelesBetween(LocalDateTime startDate, LocalDateTime endDate, Site site);
 }
