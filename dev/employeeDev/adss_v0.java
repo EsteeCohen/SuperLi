358b6cd125b0a3a59fd0abb1c9db0cd32f2d5dfb
Naama <shlittna@post.bgu.ac.il>
2025-06-05 18:12:32 +0300

    work

--- dev/employeeDev/adss_v0.java
+++ dev/employeeDev/adss_v0.java
@@ -1,12 +1,9 @@
 package employeeDev;
 
-import employeeDev.src.presentationLayer.HRSystemUI;
-
 public class adss_v0{
 	public static void main(String[] args)
 	{
-		HRSystemUI hrSystemUI = new HRSystemUI();
-		hrSystemUI.start();
+		System.out.println("Hello world!");
 	}
 }
 
